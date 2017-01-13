a6a021a4832403d5bffe14774f49439ab96538cc
Danie Ferreira <daniel.ferreira@imqs.co.za>
2017-01-13 15:02:14 +0200

    Upgraded to bootstrap 4-alpha-6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,17 @@ import { AuthService } from './auth/auth-service';
 import { AngularFire, FirebaseAuthState } from 'angularfire2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  private title = 'Go-bat';
-  private authState: FirebaseAuthState = null;
+	private title = 'Go-bat';
+	private authState: FirebaseAuthState = null;
 
-  constructor(public auth: AuthService) {
-    // this.af.auth.subscribe((state: FirebaseAuthState) => {
-    // 	this.authState = state;
-    // });
-  }
+	constructor(public auth: AuthService) {
+		// this.af.auth.subscribe((state: FirebaseAuthState) => {
+		// 	this.authState = state;
+		// });
+	}
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -4,72 +4,72 @@ import { AuthService } from '../auth/auth-service';
 import { Player } from '../player';
 
 @Component({
-  selector: 'app-player',
-  templateUrl: 'player.component.html',
-  styleUrls: ['player.component.css']
+	selector: 'app-player',
+	templateUrl: 'player.component.html',
+	styleUrls: ['player.component.css']
 })
 export class PlayerComponent {
-  newplayer: string = '';
-  loading: boolean = true;
-  isViewTable: boolean = false;
-  canAddPlayer: boolean = false;
-  players: FirebaseListObservable<any[]>;
-  playerQueue: FirebaseObjectObservable<any[]>;
-  auth: AuthService;
-  countries: string[] = ['ZA', 'ZW', 'AU', 'NZ', 'GB', 'IN', 'LK', 'PK'];
+	newplayer: string = '';
+	loading: boolean = true;
+	isViewTable: boolean = false;
+	canAddPlayer: boolean = false;
+	players: FirebaseListObservable<any[]>;
+	playerQueue: FirebaseObjectObservable<any[]>;
+	auth: AuthService;
+	countries: string[] = ['ZA', 'ZW', 'AU', 'NZ', 'GB', 'IN', 'LK', 'PK'];
 
-  constructor(public af: AngularFire, auth: AuthService) {
-    this.auth = auth;
-    this.players = af.database.list(`/players/${auth.id}`);
-    this.players.subscribe((p) => {
-      this.loading = false;
-    });
-    this.playerQueue = af.database.object(`/queues/players/${auth.id}`);
+	constructor(public af: AngularFire, auth: AuthService) {
+		this.auth = auth;
+		this.players = af.database.list(`/players/${auth.id}`);
+		this.players.subscribe((p) => {
+			this.loading = false;
+		});
+		this.playerQueue = af.database.object(`/queues/players/${auth.id}`);
 
-    // If a team already exist, prevent from adding another one
-    this.playerQueue.subscribe((q) => {
-      if ((q['$value'] === null)) {
-        this.canAddPlayer = false;
-      } else {
-        this.canAddPlayer = true;
-      }
-    });
-  }
+		// If a team already exist, prevent from adding another one
+		this.playerQueue.subscribe((q) => {
+			if ((q['$value'] === null)) {
+				this.canAddPlayer = false;
+			} else {
+				this.canAddPlayer = true;
+			}
+		});
+	}
 
-  addPlayer() {
-    const afTeam = this.af.database.object(`teams/${this.auth.id}`);
-    afTeam.subscribe((team) => {
-      const teamRegion = team.region;
-      this.playerQueue
-        .set({
-          user: this.auth.id,
-          region: teamRegion,
-          created: new Date().getTime()
-        });
-    });
-  }
+	addPlayer() {
+		const afTeam = this.af.database.object(`teams/${this.auth.id}`);
+		afTeam.subscribe((team) => {
+			const teamRegion = team.region;
+			this.playerQueue
+				.set({
+					user: this.auth.id,
+					region: teamRegion,
+					created: new Date().getTime()
+				});
+		});
+	}
 
-  deletePlayer(key: string) {
-    this.players.remove(key);
-  }
+	deletePlayer(key: string) {
+		this.players.remove(key);
+	}
 
-  // login() {
-  //   this.af.auth.login();
-  // }
+	// login() {
+	//   this.af.auth.login();
+	// }
 
-  setView(view: string) {
-    this.isViewTable = view === 'table';
-  }
+	setView(view: string) {
+		this.isViewTable = view === 'table';
+	}
 
 
 
-  //   update(key: string, newSize: string) {
-  //     this.items.update(key, { size: newSize });
-  //   }
+	//   update(key: string, newSize: string) {
+	//     this.items.update(key, { size: newSize });
+	//   }
 
-  //   deleteEverything() {
-  //     this.items.remove();
-  //   }
+	//   deleteEverything() {
+	//     this.items.remove();
+	//   }
 
 
 
